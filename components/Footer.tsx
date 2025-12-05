1e16f931f1c4e8044d29b89f3b4d538d86d2231c
Martin Carrier <martincarrier1974@users.noreply.github.com>
2025-12-05 11:46:33 -0500

    Increase footer margin to lower the black bar

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ import Logo from './Logo'
 
 export default function Footer() {
   return (
-    <footer className="bg-secondary text-white mt-16">
+    <footer className="bg-secondary text-white mt-24">
       <div className="container mx-auto px-4 py-12">
         <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
           <div>
