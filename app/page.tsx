c2a31a3dd2d102b6457d48a5713ac054aa68fe4b
Martin Carrier <martincarrier1974@users.noreply.github.com>
2025-12-05 11:57:59 -0500

    Remove Hero section - black bar

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import Hero from '@/components/Hero'
 import ProductGrid from '@/components/ProductGrid'
 import Features from '@/components/Features'
 import CTA from '@/components/CTA'
@@ -6,7 +5,6 @@ import CTA from '@/components/CTA'
 export default function Home() {
   return (
     <div>
-      <Hero />
       <Features />
       <ProductGrid />
       <CTA />
